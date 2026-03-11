9dfc53d43831b05ac2021b8313863e86f64bb0d0
Vivek <vivekkhanke123@gmail.com>
2026-03-11 07:51:29 +0530

    Updated output.py file

--- Python/day3_Output.py
+++ Python/day3_Output.py
@@ -1,18 +1,18 @@
-# # Output formatting
+# Output formatting
 
-# name = input("Enter the Name : ")
-# age = 25
+name = input("Enter the Name : ")
+age = 25
 
-# print("Name : ",name, "Age : ",age)
-# # My name is Vivek and I am 26 years old
-# print(f"My name is {name} and I am {age} years old")
-# x=5
-# y=8
+print("Name : ",name, "Age : ",age)
+# My name is Vivek and I am 26 years old
+print(f"My name is {name} and I am {age} years old")
+x=5
+y=8
 
-# x=int(input("Enter the value of X : "))
-# y=int(input("Enter the value of Y : "))
-# # Addditiojn of 25 and 23 is : 48
-# print(f"Addition of {x} and {y} is {x+y}")
+x=int(input("Enter the value of X : "))
+y=int(input("Enter the value of Y : "))
+# Addditiojn of 25 and 23 is : 48
+print(f"Addition of {x} and {y} is {x+y}")
 
 # Multiple inputs in one line
 a,b,c,d = map(int, input("Enter the number : ").split() ) # 21 32 25 21
